b9369b71dd29224e68bf88853c0844142c421a83
ryan-matroid <ryan@thematroid.com>
2018-08-22 11:33:29 -0700

    Remove mocks.

--- tests/test_client.py
+++ tests/test_client.py
@@ -10,32 +10,36 @@ from matroid.client import MatroidAPI
 SAMPLE_CAT_IMAGE_URL = 'https://www.matroid.com/images/solutions/cat.jpg'
 # SAMPLE_VIDEO_URL = 'https://www.youtube.com/watch?v=N8T6AqYKRpA'
 
-@patch('matroid.client.requests.get')
-def test_account_info(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_account_info(mock_get):
+def test_account_info():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     response = client.account_info()
     assert_is_not_none(response)
 
-@patch('matroid.client.requests.get')
-def test_list_user_detectors(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_list_user_detectors(mock_get):
+def test_list_user_detectors():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     response = client.list_detectors(**{})
     assert_is_not_none(response)
     assert(all(detector['owner'] == True  for detector in response))
 
-@patch('matroid.client.requests.get')
-def test_list_public_detectors(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_list_public_detectors(mock_get):
+def test_list_public_detectors():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     detectors = client.list_detectors(**{'published': True})
     assert_is_not_none(detectors)
     assert(all(detector['permission_level'] == 'open' or detector['permission_level'] == 'readonly' for detector in detectors))
 
-@patch('matroid.client.requests.get')
-def test_list_public_detectors_by_id(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_list_public_detectors_by_id(mock_get):
+def test_list_public_detectors_by_id():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     detectors = client.list_detectors(**{'published': True})
     assert_is_not_none(detectors)
@@ -46,27 +50,30 @@ def test_list_public_detectors_by_id(mock_get):
     detector_from_id = detector_from_id[0]
     assert(detector_from_id['name'] == detector['name'])
 
-@patch('matroid.client.requests.get')
-def test_list_public_detectors_by_label(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_list_public_detectors_by_label(mock_get):
+def test_list_public_detectors_by_label():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     detectors = client.list_detectors(**{'published': True, 'label': 'person'})
     assert_is_not_none(detectors)
     detector = detectors[0]
     assert(any(label.startswith('person') for label in detector['labels']))
 
-@patch('matroid.client.requests.get')
-def test_list_public_detectors_by_type(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_list_public_detectors_by_type(mock_get):
+def test_list_public_detectors_by_type():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     detectors = client.list_detectors(**{'published': True, 'detector_type': 'facial_characteristics'})
     assert_is_not_none(detectors)
     detector = detectors[0]
     assert(detector['type'] == 'facial_characteristics')
 
-@patch('matroid.client.requests.get')
-def test_detector_info(mock_get):
-    mock_get.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# def test_detector_info(mock_get):
+def test_detector_info():
+    # mock_get.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     detectors = client.list_detectors(**{'published': True, 'label': 'cat'})
     assert_is_not_none(detectors)
@@ -75,11 +82,12 @@ def test_detector_info(mock_get):
     assert_is_not_none(detector)
     assert(detector['detector']['name'] == detectors[0]['name'])
 
-@patch('matroid.client.requests.get')
-@patch('matroid.client.requests.post')
-def test_classify_cat_image(mock_get, mock_post):
-    mock_get.return_value.ok = True
-    mock_post.return_value.ok = True
+# @patch('matroid.client.requests.get')
+# @patch('matroid.client.requests.post')
+# def test_classify_cat_image(mock_get, mock_post):
+def test_classify_cat_image():
+    # mock_get.return_value.ok = True
+    # mock_post.return_value.ok = True
     client = MatroidAPI(options={'json_format': True})
     detectors = client.list_detectors(**{'published': True, 'name': 'Yolo VOC detector'})
     assert_is_not_none(detectors)
