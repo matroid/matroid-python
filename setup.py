353dd63dc05cda794e2e1c9d07d7531dff25f033
brendan-matroid <brendan.graham@matroid.com>
2020-06-17 20:21:52 -0400

    Fix version import

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
-from version import VERSION
 import os
 import sys
 
 try:
-  from setuptools import setup
+    from setuptools import setup
 except ImportError:
-  from distutils.core import setup
+    from distutils.core import setup
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'matroid'))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "matroid"))
+from version import VERSION
 
 setup(
-    name='matroid',
+    name="matroid",
     version=VERSION,
-    description='Matroid API Python Library',
-    author='Matroid',
-    author_email='support@matroid.com',
-    url='https://github.com/matroid/matroid-python',
-    install_requires=['requests'],
-    packages=['matroid'],
-    use_2to3=True
+    description="Matroid API Python Library",
+    author="Matroid",
+    author_email="support@matroid.com",
+    url="https://github.com/matroid/matroid-python",
+    install_requires=["requests"],
+    packages=["matroid"],
+    use_2to3=True,
 )
