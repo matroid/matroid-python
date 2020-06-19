c5c8b30a0ca1b0294cae39d1cdc084c10ab10d51
tak-matroid <tak@matroid.com>
2020-06-19 00:14:12 +0000

    fix setup error in python3

--- setup.py
+++ setup.py
@@ -1,10 +1,7 @@
 import os
 import sys
 
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
+from setuptools import setup, find_packages
 
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "matroid"))
 from version import VERSION
@@ -17,6 +14,6 @@ setup(
     author_email="support@matroid.com",
     url="https://github.com/matroid/matroid-python",
     install_requires=["requests"],
-    packages=["matroid"],
+    packages=find_packages(),
     use_2to3=True,
 )
