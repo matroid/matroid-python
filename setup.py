3362ad32e9d385e7eb09b57b5978a3cef1276ae4
jack-matroid <jack.jin@matroid.com>
2021-10-18 21:20:36 +0000

    Increase version to 1.1.1 and fix package.

--- setup.py
+++ setup.py
@@ -4,10 +4,7 @@ import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'matroid'))
 from version import VERSION
 
-try:
-  from setuptools import setup
-except ImportError:
-  from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='matroid',
@@ -17,6 +14,6 @@ setup(
     author_email='support@matroid.com',
     url='https://github.com/matroid/matroid-python',
     install_requires=['requests'],
-    packages=['matroid'],
+    packages=find_packages(),
     use_2to3=True
 )
